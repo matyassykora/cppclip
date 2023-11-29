30517e4619ac8472920ed83ac571650994038d22
matyasssykora <96580182+matyassykora@users.noreply.github.com>
2023-11-29 20:24:27 +0100

    rework

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <algorithm>
 #include <cstdlib>
-#include <iomanip>
-#include <ios>
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -15,9 +12,36 @@ public:
     this->programName = programName;
   };
 
-  template <typename... Args> ArgumentParser &add(Args... args) {
+  ArgumentParser &add(const std::string &option,
+                      const std::string &longOpt = "") {
     id++;
-    (a[id].options.push_back(args), ...);
+
+    // positional
+    if (option.at(0) != '-') {
+      a[id].positionalOpt = option;
+      a[id].isPositional = true;
+      a[id].nargs = 1;
+      return *this;
+    }
+
+    // only long
+    if (option.find("--") != std::string::npos) {
+      a[id].longOpt = option;
+      return *this;
+    }
+
+    // both long and short
+    if (!longOpt.empty()) {
+      a[id].shortOpt = option;
+      a[id].longOpt = longOpt;
+      return *this;
+    }
+
+    // only short
+    if (!option.empty()) {
+      a[id].shortOpt = option;
+    }
+
     return *this;
   }
 
@@ -27,18 +51,15 @@ public:
   }
 
   ArgumentParser &nargs(int nargs) {
-    if (nargs < 1) {
-      return *this;
-    }
-    a[id].positional = true;
     a[id].nargs = nargs;
+    a[id].isPositional = true;
     return *this;
   }
 
-  ArgumentParser &metavar(const std::string &metavar) {
-    a[id].metavar = metavar;
-    return *this;
-  }
+  // ArgumentParser &metavar(const std::string &metavar) {
+  //   a[id].metavar = metavar;
+  //   return *this;
+  // }
 
   void addDescription(const std::string &description) {
     this->description = description;
@@ -46,147 +67,162 @@ public:
 
   void addEpilogue(const std::string &epilogue) { this->epilogue = epilogue; }
 
-  void printHelp() {
-    std::cout << "Usage: " << programName << " [-h] ";
-    std::vector<int> positionalIDs;
-    for (const std::pair<const int, argument> &i : a) {
-      if (i.second.positional) {
-        positionalIDs.push_back(i.first);
-        std::cout << i.second.options.front();
-        for (int j = 0; j < i.second.nargs; j++) {
-          std::cout << ' ' << i.second.metavar;
-        }
-        std::cout << ' ';
-        continue;
-      }
-      std::cout << "[" << i.second.options.front() << "] ";
+  void parse(const int &argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+      this->args.push_back(std::string(argv[i]));
     }
-
-    if (!this->description.empty()) {
-      std::cout << "\n\n" << description;
-    }
-
-    if (!positionalIDs.empty()) {
-      std::cout << "\n\nPositional arguments:\n";
-      for (const int &i : positionalIDs) {
-        std::stringstream positionalStream;
-        positionalStream << "  ";
-        int k = 0;
-        for (const std::string &j : a[i].options) {
-          positionalStream << j;
-          k++;
-          if (k != a[i].options.size()) {
-            positionalStream << ", ";
+    for (const auto &arg : args) {
+      for (const char &c : arg) {
+        for (auto &pair : a) {
+          if (pair.second.shortOpt.size() <= 1) {
+            continue;
+          }
+          if (pair.second.shortOpt.at(1) == c) {
+            pair.second.isSet = true;
           }
         }
-        std::cout << std::setw(30) << std::left << positionalStream.str()
-                  << std::right << a[i].helpMessage << "\n";
-      }
-
-      std::cout << "\nOptional arguments:\n";
-    } else {
-      std::cout << "\n\nOptional arguments:\n";
-    }
-
-    std::cout << std::setw(30) << std::left << "  -h, --help" << std::left
-              << "show help and exit\n";
-
-    for (const std::pair<const int, argument> &i : a) {
-      std::stringstream optionStream;
-      std::stringstream helpStream;
-      optionStream << "  ";
-      for (int j = 0; j < i.second.options.size(); j++) {
-        if (i.second.positional) {
-          continue;
-        }
-        optionStream << i.second.options.at(j);
-        if (j != i.second.options.size() - 1) {
-          optionStream << ", ";
-        }
-      }
-
-      if (i.second.helpMessage.empty()) {
-        std::cout << std::setw(30) << std::left << optionStream.str() << "\n";
-      } else {
-        if (i.second.positional) {
-          continue;
-        }
-        helpStream << i.second.helpMessage << "\n";
-        std::cout << std::setw(30) << std::left << optionStream.str()
-                  << std::right << helpStream.str();
       }
     }
+  }
+
+  std::vector<std::string> getPositional(const std::string &option) {
+    std::vector<std::string> vec;
+    std::vector<std::string> all = getAllPositional();
+    for (auto i : a) {
+      if (!i.second.isPositional) {
+        continue;
+      }
+      if (option != i.second.positionalOpt) {
+        continue;
+      }
+      for (int j = 0; j < i.second.nargs; j++) {
+        if (positionalIndex >= all.size()) {
+          std::cout << "\nNot enough arguments to \"" << i.second.positionalOpt
+                    << "\"!\n";
+          exit(1);
+        }
+        vec.push_back(all.at(positionalIndex));
+        positionalIndex++;
+      }
+    }
+
+    return vec;
+  }
+
+  bool isSet(std::string option) {
+    for (auto pair : a) {
+      if (option.size() != pair.second.shortOpt.size()) {
+        continue;
+      }
+      if (option == pair.second.shortOpt && pair.second.isSet) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void printHelp() {
+    std::cout << "Usage: " << programName << " ";
+
+    // print short options
+    for (auto i : a) {
+      if (i.second.shortOpt.empty()) {
+        continue;
+      }
+      std::cout << "[" << i.second.shortOpt << "] ";
+    }
+
+    // print long options
+    for (auto i : a) {
+      if (i.second.shortOpt.empty() && !i.second.longOpt.empty()) {
+        std::cout << "[" << i.second.longOpt << "] ";
+      }
+    }
+
+    // print positional options
+    for (auto i : a) {
+      if (i.second.positionalOpt.empty()) {
+        continue;
+      }
+      std::cout << i.second.positionalOpt << " ";
+    }
+
+    std::cout << '\n';
+
+    if (!this->description.empty()) {
+      std::cout << "\n" << description << "\n";
+    }
+
+    // std::cout << std::setw(30) << std::left << "  -h, --help" << std::left
+    //           << "show help and exit\n";
 
     if (!this->epilogue.empty()) {
       std::cout << '\n' << this->epilogue << "\n";
     }
   }
 
-  void parseArgs(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
-      this->args.push_back(std::string(argv[i]));
-    }
-  }
+  bool argsEmpty() { return args.size() == 0; }
 
-  bool optionExists(const std::string &option) {
-    return std::find(this->args.begin(), this->args.end(), option) !=
-           this->args.end();
-  }
-
-  template <typename... Args>
-  std::vector<std::string> getArgsAfter(Args... args) {
-    std::vector<std::string> arguments;
-    std::vector<std::string>::const_iterator iter;
-
-    int id = mapIDFromArgs(args...);
+  // TODO: make sure arguments get correctly consumed after running this
+  std::vector<std::string> getArgsAfter(const std::string &option) {
+    std::vector<std::string> vec;
+    std::vector<std::string>::const_iterator itr;
+    int id = mapIDFromArg(option);
     const auto &map = a[id];
     int nargs = map.nargs;
     if (id == -1) {
       std::cout << "These arguments don't exist!\n";
       exit(1);
     }
-
-    for (const std::string &option : map.options) {
-      iter = std::find(this->args.begin(), this->args.end(), option);
-      for (int i = 0; i < nargs; i++) {
-        if (iter != this->args.end() && ++iter != this->args.end()) {
-          arguments.push_back(*iter);
-        }
+    itr = std::find(this->args.begin(), this->args.end(), option);
+    for (int i = 0; i < nargs; i++) {
+      if (itr != this->args.end() && ++itr != this->args.end()) {
+        vec.push_back(*itr);
       }
     }
-
-    return arguments;
+    return vec;
   }
 
-  bool argsEmpty() { return args.size() - 1 == 0; }
-
 private:
-  template <typename... Args> int mapIDFromArgs(Args... args) {
+  int mapIDFromArg(const std::string &j) {
     int id = -1;
-    std::vector<std::string> input = {args...};
-    std::vector<std::string>::const_iterator iter;
 
     for (const std::pair<const int, argument> &i : a) {
-      for (const std::string &j : input) {
-        const std::vector<std::string> &opts = i.second.options;
-        iter = std::find(opts.begin(), opts.end(), j);
-        if (iter != opts.end()) {
-          id = i.first;
-        }
+      const std::string &opts = i.second.shortOpt;
+      if (i.second.shortOpt == j) {
+        id = i.first;
+        return id;
       }
     }
 
     return id;
   }
 
+  std::vector<std::string> getAllPositional() {
+    std::vector<std::string> vec;
+    for (auto i : args) {
+      if (i[0] != '-') {
+        vec.push_back(i);
+      }
+    }
+    return vec;
+  }
+
 private:
   struct argument {
+    std::string shortOpt = "";
+    bool isSet = false;
+    std::string longOpt = "";
+    std::string positionalOpt = "";
     std::vector<std::string> options;
-    std::string helpMessage;
-    bool positional = false;
+    std::string helpMessage = "";
+    bool isPositional = false;
     int nargs = 0;
-    std::string metavar;
+    std::string metavar = "";
+    std::vector<std::string> data;
   };
+
+  int positionalIndex = 0;
 
   std::vector<std::string> args;
   std::string description = "";
@@ -197,30 +233,51 @@ private:
   std::unordered_map<int, argument> a;
 };
 
+// TODO: check this out:
+// https://www.codeproject.com/Tips/5261900/Cplusplus-Lightweight-Parsing-Command-Line-Argumen
+// and put this into nvim config https://github.com/folke/todo-comments.nvim
+
 int main(int argc, char *argv[]) {
   ArgumentParser input("main");
-  input.parseArgs(argc, argv);
 
   input.addDescription("Test description");
   input.addEpilogue("stuff and things");
+  input.add("data").nargs(2);
   input.add("-o", "--option").help("PEPA");
-  input.add("-a", "--anything").help("ASDF").nargs(2).metavar("ANYTHING");
+  input.add("-a", "--anything").help("ASDF").nargs(2);
   input.add("-s", "--something").help("FDSA");
-  input.add("--verbose");
-  input.add("-v", "--version").help("show version and exit");
+  input.add("verbose").nargs(3);
+  input.add("-v").help("show version and exit");
+  input.add("-h", "--help").help("print help and exit");
 
-  const std::vector b = input.getArgsAfter("-a", "--anything");
-  for (auto c : b) {
-    std::cout << c << "\n";
+  input.parse(argc, argv);
+
+  auto vec = input.getArgsAfter("-a");
+  for (auto i : vec) {
+    std::cout << i << "\n";
   }
 
-  if (input.optionExists("-h") || input.optionExists("--help")) {
+  if (input.argsEmpty() || input.isSet("-h")) {
     input.printHelp();
     exit(0);
+   }
+
+  auto b = input.getPositional("data");
+  for (auto i : b) {
+    std::cout << i << "\n";
   }
 
-  if (input.argsEmpty()) {
-    input.printHelp();
-    exit(0);
+  auto c = input.getPositional("verbose");
+  for (auto i : c) {
+    std::cout << i << "\n";
+  }
+
+  auto d = input.getPositional("-a");
+  for (auto i : d) {
+    std::cout << i << "\n";
+  }
+
+  if (input.isSet("-s")) {
+    std::cout << "-s SET\n";
   }
 }
