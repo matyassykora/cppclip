416e297ea64adf0f319fb96042cf63f70657d07b
matyasssykora <96580182+matyassykora@users.noreply.github.com>
2023-12-04 10:41:03 +0100

    Update example usage

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
-/*
+
 #include "CppClip.hpp"
 #include <cstdlib>
 #include <iostream>
- const std::string VERSION = "1.0";
- */
+
+const std::string VERSION = "1.0";
+
 #include <gtest/gtest.h>
 
 // TODO: check this out:
 // https://www.codeproject.com/Tips/5261900/Cplusplus-Lightweight-Parsing-Command-Line-Argumen
 
 int main(int argc, char *argv[]) {
+  /*
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+   */
 
-/*
+
   ArgumentParser input("main");
 
   input.addDescription("Test programDescription");
   input.addEpilogue("stuff and things");
-  input.add("ip").nargs(1);
-  input.add("mask");
   input.add("subnetmask");
-  // input.add("-o", "--option").help("P");
-  // input.add("-argumentMap", "--anything").help("ASDF").nargs(2);
-  // input.add("-s", "--something").help("FDSA");
+  input.add("mask").nargs(1);
+  input.add("ip").nargs(1);
+
+  input.add("-s").help("FDSA");
   // input.add("args").nargs(3);
-  input.add("-v").help("show version and exit");
+  input.add("-v", "--version").help("show version and exit");
   input.add("-h", "--help").help("print help and exit");
 
   input.parse(argc, argv);
@@ -35,7 +37,7 @@ int main(int argc, char *argv[]) {
   //   std::cout << i << "\n";
   // }
 
-  if (input.argsEmpty() || input.isSet("-h")) {
+  if (input.isSet("-h")) {
     input.printHelp();
     exit(0);
   }
@@ -45,19 +47,27 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
-  auto b = input.getPositional("ip");
-  for (auto i: b) {
-    std::cout << i << "\n";
+  try {
+    auto b = input.getPositional("ip");
+    auto c = input.getPositional("mask");
+    auto d = input.getPositional("subnetmask");
+
+    for (const auto &i: b) {
+      std::cout << i << '\n';
+    }
+
+    for (const auto &i: c) {
+      std::cout << i << '\n';
+    }
+
+    for (const auto &i: d) {
+      std::cout << i << '\n';
+    }
+  }
+  catch (const std::exception &e) {
+    input.printHelp();
+    std::cout << '\n' << e.what() << '\n';
+    exit(1);
   }
 
-  auto c = input.getPositional("mask");
-  for (auto i: c) {
-    std::cout << i << "\n";
-  }
-
-  auto d = input.getPositional("subnetmask");
-  for (auto i: d) {
-    std::cout << i << "\n";
-  }
-*/
 }
